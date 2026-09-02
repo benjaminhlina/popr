e09af01d4ce14c0d81e2ff799c660a46658e10ea
benjaminhlina <benjamin.hlina@gmail.com>
2026-09-02 16:25:05 -0400

    Inital commit of new calc likelihood functions based on using purrr and pnorm insterad of integrate

--- R/calc_likelihood.R
+++ R/calc_likelihood.R
@@ -3,101 +3,156 @@
 #' These are functions that will calculated the likelihood
 #' of movement based on additonal parameters such as depth
 #'
-#' @param pdt a `data.frame` containing psat depth data for a single transmitter
-#' @param bathy a the file path to a bathymetric grid  "/project/bathy_grid/bathy.mat.gsl.ssgb.lon72.49.lat41.52.RData"
-#' @param ncores a `numeric` that dictates the number of cores used.
+#' @param summary_log_i data.frame of a single date's rows (as produced by
+#'   splitting `summary_log` on `date` upstream); may be 0-row or have
+#'   NA/Inf `depth_max`
+#' @param preped_bathy list from `prep_bathy_likdepth()`: `idx`, `idx_na`, `bathy`, `sdi`
+#' @param dims integer vector length 2, c(nrow, ncol) of the original bathymetry grid
+
+#' @param daily_sd_depth numeric
+#' @param time character/Date, used only for error messages
+#' @keywords internal
+calc_daily_likdepth <- function(
+  summary_log_i,
+  zbi,
+  sdi_dsd,
+  idx,
+  idx_na,
+  dims,
+  time
+) {
+  # no usable depth range that day -> uninformative likelihood
+  if (
+    nrow(summary_log_i) == 0 ||
+      is.na(summary_log_i$depth_max[1]) ||
+      is.infinite(summary_log_i$depth_max[1])
+  ) {
+    lik <- zbi
+    lik[] <- 0
+    lik[idx] <- 1
+    return(matrix(lik, nrow = dims[1], ncol = dims[2]))
+  }
+
+  pulled_range <- summary_log_i[1, c("depth_min", "depth_max")] |>
+    as.numeric() |>
+    sort()
+
+  # inflate observed depth range by tag error: +-1 m and +-1%
+  low <- pulled_range[1] - (1 + 0.01 * abs(pulled_range[1]))
+  high <- pulled_range[2] + (1 + 0.01 * abs(pulled_range[2]))
+
+  lik <- tryCatch(
+    likint3_matrix(
+      w = cbind(
+        zbi,
+        sdi_dsd,
+        low,
+        high
+      ),
+      widx = idx
+    ),
+    error = function(e) {
+      cli::cli_abort(
+        c(
+          "x" = "Depth-transmission likelihood failed for {.val {as.character(time)}}.",
+          "i" = "Depth_min/Depth_max: {.val {pulled_range[1]}} / {.val {pulled_range[2]}}"
+        ),
+        parent = e
+      )
+    }
+  )
+
+  lik_mat <- matrix(lik, nrow = dims[1], ncol = dims[2])
+  lik_mat[is.na(lik_mat)] <- 0
+  lik_mat[idx_na] <- NA
+
+  max_val <- max(lik_mat, na.rm = TRUE)
+  if (is.finite(max_val) && max_val > 0) {
+    lik_mat <- lik_mat / max_val
+  }
+  return(lik_mat)
+}
+
+#' Calculate Likelihood Depth Transmitted
+#'
+#' These are functions that will calculated the likelihood of depth occupancy based on
+#' depth values transmitted and bathymetry
+#'
+#' @param preped_log a `list` containing summary objects crated by `prep_summary_likdepth()`.
+#' @param preped_bathy a `list` containinging objects created by `prep_bathy_likdepth()`.
 #' @param daily_sd_depth a numeric value
 #'
-#' @import parallel
-#' @import doParallel
-#' @import foreach
 #' @export
-#'
+
 calc_likdepth_transmit <- function(
-  pdt,
-  bathy = NULL,
-  ncores = 4,
+  preped_log,
+  preped_bathy,
   daily_sd_depth = 0.57
 ) {
   # ctd.dir="C:/Users/pgatti/Documents/halibut/R/Data_PGalbraith/toBeLoaded/";ncores=4;weighted=FALSE; focalDim = 3;daily.sd.depth=.57; pdt=sm$sm;depth.list=depth.mix
 
-  error_df(pdt)
-  error_spatrast(bathy)
-  error_numeric(ncores)
+  # rlang::arg_match(preped_log)
+  # error_df(preped_log)
+  # error_spatrast(bathy)
+  # error_numeric(ncores)
 
-  # t0 <- round(Sys.time())
-  msg_start(chr = "depth (m) likelihood")
+  # ---- msg_start
+  t0 <- msg_start(chr = "depth (m) likelihood")
+  # grab summary table
+  summary_table <- preped_log$summary_table
 
-  msg_dates(df = pdt)
+  # make sure its ordered by date and then split it
+  sum_split <- summary_table |>
+    dplyr::arrange(date) |>
+    (\(.) split(., .$date))()
+
+  # get the totla number o fdays
+  n_days <- length(sum_split)
+
+  msg_dates(df = summary_table, n_days = n_days)
+
+  # grab indx and index na
+  idx <- preped_bathy$idx
+  idx_na <- preped_bathy$idx_na
+
+  # grab bathymetry and convert it to a vector
+  zbi <- .convert_spr_to_vec(x = preped_bathy$bathy)
+  zbi_dims <- dim(preped_bathy$bathy)
+  # grab sdi and convert to vector
+  sdi <- .convert_spr_to_vec(x = preped_bathy$sdi)
+
+  # add daily_sd_depth to sdi
+  sdi_dsd <- sdi + daily_sd_depth
 
   #---------------------------------------------------------------------
   # start of heavy daily loop
   # here is the place to implement parallelisation
   #--------------------------
 
-  cli::cli_alert("Starting iterations through deployment period....")
-
-  # declare cluster parrallel
-  cl <- parallel::makeCluster(ncores)
-  doParallel::registerDoParallel(cl, cores = ncores)
-  ans <- foreach::foreach(i = 1:T) %dopar%
-    {
-      # i = 2
-      # i=187
-      # i=341
-      # time <- as.Date(ac(dateVec[i])) #as.Date(udates[i])
-      # pdt.i <- pdt[ac(pdt$day) == ac(time), ]
-      # range fish depth considered to be the bottom
-      # error tag +-1m +-1% depth
-      if (is.na(pdt.i$Depth_max) | is.infinite(pdt.i$Depth_max)) {
-        lik <- zbi
-        lik[inds] <- 1
-      } else {
-        zz <- sort(an(pdt.i[, c('Depth_min', 'Depth_max')]))
-        df <- cbind.data.frame(
-          low = zz[1] - (1 + .01 * abs(zz[1])),
-          high = zz[2] + (1 + .01 * abs(zz[2]))
-        )
-
-        # indexes
-
-        # lik gauss
-        likint3_matrix()
-        lik <- likint3_matrix(
-          w = cbind(
-            as.vector(zbi),
-            as.vector(sdi) + daily.sd.depth,
-            an(df[1]),
-            an(df[2])
-          ),
-          widx = inds
-        )
-        lik <- matrix(data = lik, ncol = ncol(zbi), nrow = nrow(zbi))
-
-        # lik
-        lik[is.na(lik)] <- 0
-        lik[indNA] <- NA
-        lik <- lik / max(lik, na.rm = TRUE)
-      }
-      lik
-    }
-  # end of heavy daily loop
-  parallel::stopCluster(cl)
-  #---------------------------------------------------------------------
-  # array of likelihood (empty)
-  L. <- array(0, dim = c(dim(zbi)[1], dim(zbi)[2], length(dateVec)))
-  # switch from list to array
-  for (i in 1:T) {
-    L.[,, i] <- ans[[i]]
+  lik_list <- purrr::imap(
+    sum_split,
+    function(summary_log_i, date_i) {
+      calc_daily_likdepth(
+        summary_log_i = summary_log_i,
+        zbi = zbi,
+        sdi_dsd = sdi_dsd,
+        idx = idx,
+        idx_na = idx_na,
+        dims = zbi_dims,
+        time = date_i
+      )
+    },
+    .progress = "Calculating depth likelihood"
+  )
+  likeihood_array <- array(0, dim = c(zbi_dims[1], zbi_dims[2], n_days))
+  for (i in seq_len(n_days)) {
+    likeihood_array[,, i] <- lik_list[[i]]
   }
+  likeihood_array <- aperm(likeihood_array, c(2, 1, 3))
 
-  L. <- aperm(L., c(2, 1, 3))
-  t1 <- Sys.time()
-  print(paste(
-    "CTD profile calculations took ",
-    round(as.numeric(difftime(t1, t0, units = "mins")), 2),
-    "minutes..."
-  ))
-  #options(warn = 2)
-  return(L.)
+  cli::cli_alert_success(
+    "Depth-transmission likelihood done in {round(as.numeric(difftime(Sys.time(), t0, units = 'mins')), 2)} min"
+  )
+
+  return(likeihood_array)
 }
